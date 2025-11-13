59f9870f72008f5f26cc403cd2bf1e18ad48cfe3
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2025-11-13 11:16:18 +0100

    Create post by user

--- src/home.js
+++ src/home.js
@@ -1,28 +1,30 @@
 axios.get('https://tarmeezacademy.com/api/v1/posts')
   .then(response => {
-    let posts = document.getElementById('posts')
-    let postsCount = response.data.data.length - 1
-    for (let i = 0; i <= postsCount; i++) {
-        let content = `
-            <div class="card my-5">
-                <div class="card-header">
-                    <a class="navbar-brand" href="#"><img class="rounded-circle mx-2" style="width: 30px" src="${response.data.data[1].author.profile_image}"></a>
-                    <b>${response.data.data[i].author.username}</b>
-                </div>
-                <div class="card-body">
-                    <img class="w-100" src="${response.data.data[i].image}">
-                    <p class="text-secondary">${response.data.data[i].created_at}</p>
-                    <h5 class="card-title">${response.data.data[1].title}</h5>
-                    <p class="card-text">${response.data.data[1].body}</p>
-                    <hr>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16"><path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/></svg>
-                    <span class="mx-2">(${response.data.data[1].comments_count}) Comments</span>
-                </div>
-            </div>        
-        `
-        posts.innerHTML += content
+    function getPosts() {
+      let posts = document.getElementById('posts')
+      let postsCount = response.data.data.length - 1
+      for (let i = 0; i <= postsCount; i++) {
+          let content = `
+              <div class="card my-5">
+                  <div class="card-header">
+                      <a class="navbar-brand" href="#"><img class="rounded-circle mx-2" style="width: 30px" src="${response.data.data[1].author.profile_image}"></a>
+                      <b>${response.data.data[i].author.username}</b>
+                  </div>
+                  <div class="card-body">
+                      <img class="w-100" src="${response.data.data[i].image}">
+                      <p class="text-secondary">${response.data.data[i].created_at}</p>
+                      <h5 class="card-title">${response.data.data[i].title}</h5>
+                      <p class="card-text">${response.data.data[i].body}</p>
+                      <hr>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16"><path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/></svg>
+                      <span class="mx-2">(${response.data.data[i].comments_count}) Comments</span>
+                  </div>
+              </div>        
+          `
+          posts.innerHTML += content
+      }
     }
-
+    getPosts()
   })
   .catch(error => {
     console.log(error)
@@ -68,7 +70,8 @@ function loginUser() {
     navProfileImg.classList.remove('d-none')
     navProfileImg.classList.add('d-block')
 
-    loginModal.remove()
+    const modal = bootstrap.Modal.getInstance(loginModal)
+    modal.hide()
     
     loginSucceed.classList.remove('d-none')
     loginSucceed.classList.add('d-block')
@@ -79,6 +82,8 @@ function loginUser() {
   })
   .catch(error => {
     console.log(error)
+    const modal = bootstrap.Modal.getInstance(loginModal)
+    modal.hide()
     loginFailed.classList.remove('d-none')
     loginFailed.classList.add('d-block')
     setTimeout(function(){
@@ -114,8 +119,9 @@ function registerUser() {
     navProfileImg.classList.remove('d-none')
     navProfileImg.classList.add('d-block')
 
-    registerModal.remove()
-    
+    const modal = bootstrap.Modal.getInstance(registerModal)
+    modal.hide()
+
     registerSucceed.classList.remove('d-none')
     registerSucceed.classList.add('d-block')
     setTimeout(function(){
@@ -124,8 +130,9 @@ function registerUser() {
     }, 2000)
   })
   .catch(error => {
-    registerModal.remove()
-  
+    const modal = bootstrap.Modal.getInstance(registerModal)
+    modal.hide()
+
     registerFailed.classList.remove('d-none')
     registerFailed.classList.add('d-block')
     
@@ -136,5 +143,64 @@ function registerUser() {
       registerFailed.classList.remove('d-block')
       registerFailed.classList.add('d-none')
     }, 2000)
+  })
+}
+
+logoutBtn.addEventListener('click', function() {
+  loginBtn.classList.remove('d-none')
+  loginBtn.classList.add('d-block')
+  registerBtn.classList.remove('d-none')
+  registerBtn.classList.add('d-block')
+  logoutBtn.classList.remove('d-block')
+  logoutBtn.classList.add('d-none')
+
+  addPostBtn.classList.remove('d-block')
+  addPostBtn.classList.add('d-none')
+
+  navLogo.classList.remove('d-none')
+  navLogo.classList.add('d-block')
+  navProfileImg.classList.remove('d-block')
+  navProfileImg.classList.add('d-none')
+})
+
+const createPostSucceed = document.getElementById('createPost-succeed')
+const createPostFailed = document.getElementById('createPost-failed')
+
+const postModal = document.getElementById('postModal')
+
+function createPost() {
+  const postTitle = document.getElementById('post-title').value
+  const postBody = document.getElementById('post-body').value
+  const postImage = document.getElementById('post-image').files[0]
+  const token = localStorage.getItem('token')
+  
+  const formData = new FormData()
+  formData.append("title" ,postTitle)
+  formData.append("body" ,postBody)
+  formData.append("image" ,postImage)
+
+  const header = {
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  }
+
+  axios.post('https://tarmeezacademy.com/api/v1/posts', formData, header)
+  .then(function (response) {
+    console.log(response)
+    getPosts()
+    createPostSucceed.classList.remove('d-none')
+    createPostSucceed.classList.add('d-block')
+
+    const modal = bootstrap.Modal.getInstance(postModal)
+    modal.hide()
+  })
+  .catch(function (error) {
+      createPostFailed.textContent = error.response.data.message
+      createPostFailed.classList.remove('d-none')
+      createPostFailed.classList.add('d-block')
+
+      const modal = bootstrap.Modal.getInstance(postModal)
+      modal.hide()
   })
 }
